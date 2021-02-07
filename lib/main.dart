5df9a148a22d979c05503807fac18628142f7070
Lele660 <60441401+Lele660@users.noreply.github.com>
2021-02-07 11:50:02 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'db_services/user_db.dart';
 
-void main() {
+void main() async {
+  await UserDb.syncUserMap();
   runApp(MyApp());
 }
