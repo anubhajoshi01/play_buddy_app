f38f4c2ce46b62d7bd2afddcccd3e29dc419bffc
Lele660 <60441401+Lele660@users.noreply.github.com>
2021-02-09 12:02:08 -0500

    some interface

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frc_challenge_app/components/common_app_bar.dart';
 
-class CreatePostScreen extends StatefulWidget{
+class CreatePostScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -10,13 +10,115 @@ class CreatePostScreen extends StatefulWidget{
   }
 }
 
-class _CreatePostScreen extends State<CreatePostScreen>{
+class _CreatePostScreen extends State<CreatePostScreen> {
+  String descriptionStr = "";
+  String infoStr = "";
+  String activity = "";
+  String dropdownValue = "event date";
+  static final List<int> years = [2021, 2022, 2023, 2024];
+  static final List<int> months = [
+    01,
+    02,
+    03,
+    04,
+    05,
+    06,
+    07,
+    08,
+    09,
+    10,
+    11,
+    12
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommonAppBar.appBar("Create Post"),
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Create a Post"),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+              child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: Text(
+              "Enter the activity type: ",
+              style: TextStyle(
+                color: Colors.grey[900],
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(3),
+            color: Colors.white,
+            child: TextField(
+              keyboardType: TextInputType.text,
+              onChanged: (input) {
+                setState(() {
+                  infoStr = input;
+                });
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: Text(
+              "Enter the descripton of the event:  ",
+              style: TextStyle(
+                color: Colors.grey[900],
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(3),
+            color: Colors.white,
+            child: TextField(
+              keyboardType: TextInputType.text,
+              onChanged: (input) {
+                setState(() {
+                  descriptionStr = input;
+                });
+              },
+            ),
+          ),
+          Container(
+              padding: EdgeInsets.symmetric(vertical: 40),
+              child: DropdownButton<String>(
+                value: dropdownValue,
+                icon: Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String newValue) {
+                  setState(() {
+                    dropdownValue = newValue;
+                  });
+                },
+                items: <String>['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              )),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 300.0, horizontal: 5.0),
+          ),
+        ],
+      ))),
     );
   }
 }
