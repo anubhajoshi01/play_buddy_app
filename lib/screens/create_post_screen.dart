d5dcbd0740614528d4121c7f1d3d3adc3e50cccd
Anubha Joshi <anubhajoshi01@gmail.com>
2021-02-11 16:43:16 -0500

    unknown changes

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -23,7 +23,7 @@ class _CreatePostScreen extends State<CreatePostScreen> {
   static final List years = ["2021", "2022", "2023", "2024"];
 
   static final List months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-  static DateTime selectedDate;
+  static DateTime selectedDate = DateTime.now();
 
   _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
