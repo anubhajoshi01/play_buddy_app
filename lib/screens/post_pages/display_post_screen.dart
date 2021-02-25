726e338b142d9d3a85f4da9aba34e2ea0aebef28
Lele660 <60441401+Lele660@users.noreply.github.com>
2021-02-25 16:31:23 -0500

    datetime format

--- lib/screens/post_pages/display_post_screen.dart
+++ lib/screens/post_pages/display_post_screen.dart
@@ -27,6 +27,9 @@ class _DisplayPostScreen extends State<DisplayPostScreen> {
   int numSignedUp;
   bool signedUp;
   bool owned;
+  DateTime t;
+  String time;
+
 
   @override
   void initState() {
@@ -38,6 +41,9 @@ class _DisplayPostScreen extends State<DisplayPostScreen> {
         .contains(this.widget.post.id);
     int thisUser = UserDb.userMap[UserDb.emailMap[EmailDb.thisEmail]].id;
     owned = this.widget.post.ownerUserId == thisUser;
+    t = this.widget.post.eventDateTime;
+    time = "{${t.hour.toString()}-${t.minute.toString()}";
+    print("time is ${time}");
   }
 
   @override
@@ -72,7 +78,7 @@ class _DisplayPostScreen extends State<DisplayPostScreen> {
                   )),
               Text("${this.widget.post.eventDescription}"),
               Text("\n"),
-              Text("${this.widget.post.eventDateTime.toString()}"),
+              Text("$time"),
               Text("\n"),
               Text("${this.widget.post.address}"),
               Text("\n"),
