9b69c93345a832643f3a57f0cdab38319f62d4eb
unknown <rkwan.hy@gmail.com>
2021-02-28 14:41:44 -0500

    cards

--- lib/screens/post_pages/post_map_screen.dart
+++ lib/screens/post_pages/post_map_screen.dart
@@ -245,6 +245,7 @@ class _PostMapScreen extends State<PostMapScreen> {
                                 ),
                               )
                             : Container();
+
                       }))
         ],
       )),
